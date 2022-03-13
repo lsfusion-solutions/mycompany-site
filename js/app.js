c18590468310ba280c93459ab57cb6bfce0bc85a
aramanenka <sasha.romanenko@gmail.com>
2022-03-13 20:56:23 +0300

    Header and footer updates

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
+    $(document).click(function(e){
 
+        let target = e.target;
+
+        if( $(target).is(".language") || $(target).closest(".language").size() > 0 ){
+            $("div.language").toggleClass("active");
+        }else{
+            $("div.language").removeClass("active");
+        }
+    })
+    $(".menu-link").click(function(e){$(this).toggleClass("active");e.preventDefault()})
 })
