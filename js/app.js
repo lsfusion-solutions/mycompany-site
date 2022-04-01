c2c9a00363774614f6ac34933b34d26b93077a1f
aramanenka <sasha.romanenko@gmail.com>
2022-04-01 12:43:55 +0300

    Header and footer updates

--- js/app.js
+++ js/app.js
@@ -10,10 +10,19 @@ $(document).ready(function() {
         }
     })
     $(".menu-link").click(function(e){$(this).toggleClass("active");e.preventDefault()})
+
     $("#section-work nav .for_developers").click(function(){
         $(this).closest(".inner").removeClass("for_administrators").addClass("for_developers")
     })
     $("#section-work nav .for_administrators").click(function(){
         $(this).closest(".inner").removeClass("for_developers").addClass("for_administrators")
     })
+
+    $("#section-easy nav .for_users").click(function(){
+        $(this).closest(".inner").removeClass("for_business").addClass("for_users")
+    })
+    $("#section-easy nav .for_business").click(function(){
+        $(this).closest(".inner").removeClass("for_users").addClass("for_business")
+    })
+
 })
