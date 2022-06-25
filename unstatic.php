90cada90590b869091c27a6c2c10ad0f608a48eb
aramanenka <sasha.romanenko@gmail.com>
2022-06-25 09:17:21 +0300

    Added contact us form

---
+++ unstatic.php
@@ -0,0 +1,58 @@
+<?php
+
+    $allowerEmails = array("sasha.romanenko@gmail.com", "anotheremail@gmail.com");
+    $defaultEmails = array("sasha.romanenko@gmail.com");
+
+    $replaceSign = "#";
+
+    $subject = "Новый запрос (" .date("Y-m-d H:i:s") . ")";
+
+
+    $message = "";
+
+    foreach ($_POST as $_name => $_value){
+        switch( $_name ){
+            case "subject":
+                $subject = $_value . " (" .date("Y-m-d H:i:s") . ")";
+                break;
+            case "thankyou":
+            case "to":
+                break;
+            default:
+                $message .= "<strong>{$_name}:</strong> {$_value} <br />";
+        }
+    }
+
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+
+    if( isset($_POST["to"]) ){
+
+        if( is_array($_POST["to"]) ){
+            foreach($_POST["to"] as $_email){
+                $_email = str_replace($replaceSign, "@", $_email);
+                if( in_array($_email, $allowerEmails) ){
+                    @mail($_email, $subject, $message, $headers);
+                }
+
+            }
+        }else{
+            $_email = str_replace($replaceSign, "@", $_POST["to"]);
+            if( in_array($_email, $allowerEmails) ){
+                $result = @mail($_email, $subject, $message, $headers);
+            }
+        }
+
+    }else{
+
+        foreach($defaultEmails as $_email){
+            @mail($_email, $subject, $message, $headers);
+        }
+    }
+
+    if( isset($_POST["thankyou"]) ) {
+        header("Location: {$_POST["thankyou"]}");
+    }else{
+        echo "Are you bot?";
+    }
