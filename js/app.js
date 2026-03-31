0e0b1e1adb965d87099f85c5980ee80741272a1c
aramanenka <sasha.romanenko@gmail.com>
2026-03-31 16:37:44 +0300

    Contact Us added to home page

--- js/app.js
+++ js/app.js
@@ -117,7 +117,28 @@ $(document).ready(function() {
         var match = url.match(regExp);
         return (match&&match[7].length==11)? match[7] + start : false;
     }
-    $("a.video").click(function(e){
+    function getYouTubeCode(url){
+        var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+        var match = url.match(regExp);
+        return (match&&match[7].length==11)? match[7] : false;
+    }
+    function getDzenCode(url){
+        return url;
+    }
+    function getVKCode(url){
+        if(getUrlVars(url)["z"]) {
+            let vals = getUrlVars(url)["z"].substring(5).split("_");
+            return "oid=" + vals[0] + "&id=" + vals[1];
+        }
+        //example: https://vk.com/video870533294_456239048
+        url = url.substring(url.indexOf("video") + 5);
+        let vals = url.split("_");
+        return "oid=" + vals[0] + "&id=" + vals[1];
+    }
+    $(document).on("click", "#videopopup .c", function(e){
+        $(this).closest("span.notice").hide();
+    })
+    $(document).on("click", "a.video, a.youtube, a.dzen, a.vk", function(e){
         if( $("#videopopup").length == 0 ){
             $("body").append(
                 '<div id="videopopup" class="popup"><span class="close">x</span><div class="inner"></div></div>'
@@ -129,11 +150,45 @@ $(document).ready(function() {
                 }
             });
         }
-        let _attr = "";
-        if( $(this).attr("data-autoplay") ){
-            _attr = "?autoplay=1";
+        let _html = "";
+        if($(this).attr("alt-youtube") || $(this).attr("alt-dzen") || $(this).attr("alt-vk")){
+            _noticeHTML = '<span class="notice"><em class="c" title="Скрыть">x</em>Медленно работает видео? Смотреть в';
+            if($(this).attr("alt-youtube")){
+                _noticeHTML += ' <a href="' + $(this).attr("alt-youtube") + '" class="youtube">YouTube</a>'
+            }
+            if( $(this).attr("alt-dzen") ){
+                let _id = "";
+                if( $(this).attr("data-id") ){
+                    _id = $(this).attr("data-id")
+                }else {
+                    _id = $(this).closest(".video-wrapper").find(".dzen").attr("data-id")
+                }
+
+                if(_id) {
+                    _noticeHTML += ' <a href="' + $(this).attr("alt-dzen") + '" data-id="' + _id + '" class="dzen">Дзен</a>'
+                }
+            }
+            if($(this).attr("alt-vk")){
+                _noticeHTML += ' <a href="' + $(this).attr("alt-vk") + '" class="vk">ВК</a>'
+            }
+            _noticeHTML += "</span>"
+
+            _html += _noticeHTML;
         }
-        $("#videopopup .inner").html( '<iframe src="https://www.youtube.com/embed/' + getYouTubeCode($(this).attr("href")) + _attr + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' )
+
+        $("#videopopup").removeClass("video-dzen").removeClass("video-vk").removeClass("video-youtube")
+        if($(this).is(".dzen")){
+            $("#videopopup").addClass("video-dzen")
+            _html += '<iframe src="https://dzen.ru/embed/' + getDzenCode($(this).attr("data-id")) + '?mute=0&autoplay=1&tv=0" allow="autoplay; fullscreen; accelerometer; gyroscope; picture-in-picture; encrypted-media" frameborder="0" scrolling="no" allowfullscreen></iframe>';
+        }else if($(this).is(".vk")){
+            $("#videopopup").addClass("video-vk")
+            _html += '<iframe src="https://vk.com/video_ext.php?' + getVKCode($(this).attr("href")) + '&autoplay=1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>';
+        }else{//by default it is youtube video
+            //$("#videopopup").addClass("video-youtube")
+            _html += '<iframe src="https://www.youtube.com/embed/' + getYouTubeCode($(this).attr("href")) + '?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        }
+
+        $("#videopopup .inner").html( _html )
         $("#videopopup").popup("show");
 
         e.preventDefault()
